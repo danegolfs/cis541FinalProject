be55be299a7a70850866ef642c942f3bc652d1ed
Dane Walton <dwalton@seas.upenn.edu>
2017-12-08 11:22:37 -0500

    Added pretty colors

--- server.js
+++ server.js
@@ -1,32 +1,39 @@
 const express = require('express');
 const mqtt = require('mqtt');
+const https = require('https');
 const app = express();
 var MongoClient = require('mongodb').MongoClient;
 const port = 3000;
 
 var database;
 
+const things = "demo.thingsboard.io";
+const accessToken = "U6lUUr51toZfVnx9hC0I";
+
+app.set('view engine', 'ejs');
+
+
 // Connect to the db
-MongoClient.connect("mongodb://localhost:27017/groupOne", function(err, db) {
-  if (!err) {
-    console.log("We are connected");
-  }
-  database = db;
-  // db.collection('heartRates').insertMany(docs, function(err, result) {
-  //   console.log("Inserted " + result.insertedCount + " items");
+// MongoClient.connect("mongodb://localhost:27017/groupOne", function(err, db) {
+//   if (!err) {
+//     console.log("We are connected");
+//   }
+//   database = db;
+//   // db.collection('heartRates').insertMany(docs, function(err, result) {
+//   //   console.log("Inserted " + result.insertedCount + " items");
 
-  db.collection('heartRates').find({}).toArray(function(err, items) {
-    console.log("items size: " + items.length);
-    for (x in items) {
-      console.log("found: " + items[x].heartRate);
-    }
+//   db.collection('heartRates').find({}).toArray(function(err, items) {
+//     console.log("items size: " + items.length);
+//     for (x in items) {
+//       console.log("found: " + items[x].heartRate);
+//     }
 
-  });
-});
+//   });
+// });
 
 
-var topicPost = "cis541/hw-mqtt/26013f37-2ffe00bae2a9c2852a68f57f5001e87/data";
-var topicSub = "cis541/hw-mqtt/26013f37-2ffe00bae2a9c2852a68f57f5001e87/echo";
+var topicPost = "cis541/hw-mqtt/26013f37-9ff600bae2a8ca552b0e02cf5001e85/data";
+var topicSub = "cis541/hw-mqtt/26013f37-9ff600bae2a8ca552b0e02cf5001e85/echo";
 var options = {
   cmd: 'connect',
   protocolId: 'MQTT', // Or 'MQIsdp' in MQTT 3.1.1
@@ -39,48 +46,67 @@ var options = {
 };
 
 var randNum = 0;
+var pace = 0;
+var alarm = 0;
 
 var client = mqtt.connect('mqtt:35.188.242.1:1883', options);
 
 client.on('connect', function() {
   client.subscribe(topicSub);
   console.log("Subscribed to topic");
-  client.publish(topicPost, '4');
-  console.log("Sent a 4");
 });
 
 client.on('message', function(topic, message) {
   // message is Buffer
   var insertVal = { id: 1, heartRate: message };
-  database.collection('heartRates').insert(insertVal, function(err, result) {
+  // database.collection('heartRates').insert(insertVal, function(err, result) {
 
-  });
-  database.collection('heartRates').find({}).toArray(function(err, items) {
-    console.log("items size: " + items.length);
-    for (x in items) {
-      console.log("found: " + items[x].heartRate);
-    }
-  });
-  console.log(message.toString());
+  // });
+  // database.collection('heartRates').find({}).toArray(function(err, items) {
+  //   console.log("items size: " + items.length);
+  //   for (x in items) {
+  //     console.log("found: " + items[x].heartRate);
+  //   }
+  // });
+  var messageNum = parseInt(message);
+
+  var newString = message.toString();
+  newString = newString.replace("A949BC5E-", "");
+  console.log("From mbed: " + newString);
+  if (randNum != 1) {
+    var send = "https://api.thingspeak.com/update?api_key=FJR8UXE16BXRQCYZ&field1=" + randNum;
+    console.log("Logged a " + send);
+    https.get(send, res => {});
+  }
   //client.end()
 });
 
 app.get('/data', function(req, res) {
   console.log("Got data request");
+
+  getNewData();
+
+  
+
   res.send(randNum.toString());
 });
 
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/www/index.html');
+  res.render('index');
 });
 
 function getNewData() {
-  randNum = Math.round(Math.random() * 100)
+  randNum = Math.random();
+  //Math.round(Math.random() * 100)
+  if (randNum < 0.5) randNum = 1;
+  else randNum = Math.round(randNum * 100);
   client.publish(topicPost, randNum.toString());
   console.log("Published " + randNum)
 }
 
-setInterval(getNewData, 3000);
+
+//setInterval(getNewData, 3000);
 
 
 app.listen(port, (err) => {
